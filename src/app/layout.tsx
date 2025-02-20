6c32c98f6d5a51fca15d2e27fb434f8d3be74d8d
arpitnexuses <arpit.m@nexuses.in>
2025-02-20 14:11:17 +0530

    third commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google';
+import { Metadata } from 'next';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -12,9 +13,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${inter.variable}`}>
+      <head>
+        <title>VPAC</title>
+      </head>
       <body className="font-inter">
         {children}
       </body>
     </html>
   );
+}
+
+export const metadata: Metadata = {
+  title: 'VPAC',
+  description: 'VPAC description'
 } 
